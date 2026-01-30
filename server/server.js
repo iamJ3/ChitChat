19531ed72905288cc414a7bcb9d14f6e8e96f0a6
Jatin Sharma <jatina9410@gmail.com>
2026-01-31 00:42:19 +0530

    installed and ran express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log("hey");
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/',(req,res)=>{
+    res.json({
+        msg:"hello form server"
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is listening on http://localhost:${PORT}`);
+    
+})
