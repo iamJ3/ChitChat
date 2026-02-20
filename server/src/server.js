61db0391e20200e3ac5e5946d2df895c44bb785c
Jatin Sharma <jatina9410@gmail.com>
2026-02-20 23:28:24 +0530

    fix : cors errors

--- server/src/server.js
+++ server/src/server.js
@@ -3,11 +3,16 @@ import authRoutes from './routes/auth.route.js';
 import dotenv from "dotenv"
 import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 // import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 
 dotenv.config()
+app.use(cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
